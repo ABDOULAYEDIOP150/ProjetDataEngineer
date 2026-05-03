2fe534a0f00b5863256a7ab91dcaec3f43d000ac
ABDOULAYE DIOP <117550162+ABDOULAYEDIOP150@users.noreply.github.com>
2026-05-03 22:02:11 +0200

    Update load_csv_to_postgres.py

--- ingestion/load_csv_to_postgres.py
+++ ingestion/load_csv_to_postgres.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pathlib import Path
 from sqlalchemy import text
+from sqlalchemy.exc import ProgrammingError
 
 from utils import get_postgres_engine
 
@@ -8,6 +9,9 @@ from utils import get_postgres_engine
 def load_csv_folder_to_postgres(folder_path, schema="raw"):
     engine = get_postgres_engine()
     folder = Path(folder_path)
+
+    with engine.begin() as conn:
+        conn.execute(text(f"CREATE SCHEMA IF NOT EXISTS {schema}"))
 
     for file_path in folder.glob("*.csv"):
         table_name = file_path.stem
@@ -17,17 +21,20 @@ def load_csv_folder_to_postgres(folder_path, schema="raw"):
         df = pd.read_csv(file_path)
 
         with engine.begin() as conn:
-            # 🔥 Vider la table sans la supprimer (important)
-            conn.execute(
-                text(f'TRUNCATE TABLE {schema}.{table_name} RESTART IDENTITY CASCADE')
-            )
+            try:
+                conn.execute(
+                    text(f'TRUNCATE TABLE {schema}.{table_name} RESTART IDENTITY CASCADE')
+                )
+                if_exists_mode = "append"
+            except ProgrammingError:
+                print(f"⚠️ Table {schema}.{table_name} inexistante. Création de la table.")
+                if_exists_mode = "replace"
 
-            # 🔥 Insérer les données
             df.to_sql(
                 name=table_name,
                 con=conn,
                 schema=schema,
-                if_exists="append",
+                if_exists=if_exists_mode,
                 index=False
             )
 
